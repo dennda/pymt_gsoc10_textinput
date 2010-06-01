529170e6a9a84b88e7aaeca6e56241d1a701cd15
Christopher Denter <dennda@the-space-station.com>
2010-06-01 22:06:14 +0200

    wipekeyboard: make use of spelling suggestions

--- wipekeyboard.py
+++ wipekeyboard.py
@@ -1,7 +1,10 @@
 from pymt import *
 
 
-class MTWipeKeyboard(MTVKeyboard):
+class MTWipeKeyboard(MTSpellVKeyboard):
+    def __init__(self, **kwargs):
+        super(MTWipeKeyboard, self).__init__(**kwargs)
+
     def _is_letter(self, key):
         return key in self.layout.LETTERS
 
@@ -11,6 +14,7 @@ class MTWipeKeyboard(MTVKeyboard):
             keyinfo = self.get_key_at_pos(x, y)
             # XXX Why should it be None?
             if keyinfo is not None and self._is_letter(keyinfo[0]):
+                self._clear_suggestions()
                 touch.userdata['collected_keys'] = [keyinfo]
                 # What about the active_keys?
                 return True
@@ -21,7 +25,6 @@ class MTWipeKeyboard(MTVKeyboard):
         if collected is None:
             # Touch started outside of the widget, ignore it.
             return
-
         x, y = self.to_local(*touch.pos)
         keyinfo = self.get_key_at_pos(x, y)
         if keyinfo and keyinfo != collected[-1] and self._is_letter(keyinfo[0]):
@@ -36,7 +39,22 @@ class MTWipeKeyboard(MTVKeyboard):
         collected = touch.userdata.get('collected_keys')
         if collected is not None:
             print "".join(keyinfo[0][0] for keyinfo in collected)
+            print "ANALYZED:", self.analyze_collected_keys(collected)
             return True
+
+    def analyze_collected_keys(self, keys):
+        chars = [key[0][0][0] for key in keys]
+        if len(keys) == 1:
+            word = chars[0]
+        elif len(keys) == 2:
+            word = "".join(chars)
+        # XXX Add angular threshold for wipe movement here
+        word = "".join(chars)
+        for suggestion in self.spelling.suggest(word)[:10]:
+            self._add_suggestion(suggestion)
+        if self.spelling.check(word):
+            return word
+
 
 
 if __name__ == '__main__':
